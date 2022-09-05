6fb6067e3af2101234e10ec6efe466da091e8a7d
noheezybucket <seydinag023@gmail.com>
2022-09-05 22:08:17 +0000

    final commit: animations added - site finished

--- src/App.js
+++ src/App.js
@@ -13,19 +13,15 @@ function App() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="font-perso box-border overflow-x-hidden">
+    <div className="font-perso box-border">
       <Navbar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
-      <Welcome className="overflow-x-hidden" />
-      <WhoIs className="overflow-x-hidden" />
-      <TrustUs className="overflow-x-hidden" />
-      <Services className="overflow-x-hidden" />
-      <Contact className="overflow-x-hidden" />
-      <Footer className="overflow-x-hidden" />
-      <Sidebar
-        className="overflow-x-hidden"
-        isNavOpen={isNavOpen}
-        setIsNavOpen={setIsNavOpen}
-      />
+      <Welcome />
+      <WhoIs />
+      <TrustUs />
+      <Services />
+      <Contact />
+      <Footer />
+      <Sidebar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
     </div>
   );
 }
